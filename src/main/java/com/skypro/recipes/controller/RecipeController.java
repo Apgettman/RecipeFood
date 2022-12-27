c04d4e25cb4713cd2fa91c881e8644ad78db4be9
679 <apgettman@gmail.com>
2022-12-27 22:39:30 +0300

    RecipeFood

--- src/main/java/com/skypro/recipes/controller/RecipeController.java
+++ src/main/java/com/skypro/recipes/controller/RecipeController.java
@@ -2,6 +2,8 @@ package com.skypro.recipes.controller;
 
 import com.skypro.recipes.model.Recipe;
 import com.skypro.recipes.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description = "Операции удаления, создания, получения и редактирования рецептов")
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -25,12 +28,14 @@ public class RecipeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Операция для получения рецепта")
     public Recipe getRecipe(@PathVariable("id") long id) {
 
         return recipeService.get(id);
     }
 
     @PostMapping
+    @Operation(summary = "Операция для создания рецепта")
     public ResponseEntity<?> addRecipe(@RequestBody Recipe recipe) {
         if (StringUtils.isBlank(recipe.getTitle())) {
             return ResponseEntity.badRequest().body("Название рецепта не может быть пустым");
@@ -39,11 +44,13 @@ public class RecipeController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Операция для редактирования рецепта")
     public Recipe updateRecipe(@PathVariable("id") long id, @RequestBody Recipe recipe) {
         return recipeService.upDate(id, recipe);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Операция для удаления рецепта")
     public Recipe deleteRecipe(@PathVariable("id") long id) {
         return recipeService.remove(id);
     }
